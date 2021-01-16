d9fd4b98323dd34afccc36da55a19eec016e67d6
lilin <a1b2c3d4>
2021-01-16 18:35:43 +0800

    javaSourceLearn

---
+++ src/com/company/test/hashmap/test.java
@@ -0,0 +1,13 @@
+package com.company.test.hashmap;
+
+import java.util.HashMap;
+
+public class test {
+    public static void main(String[] args) {
+        HashMap x = new HashMap();
+
+        x.put(0,"a");
+        x.put(1,"b");
+        x.put(2,"c");
+    }
+}
